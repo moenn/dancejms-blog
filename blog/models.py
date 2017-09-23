b278a35453fd43b3cef4a33f52916891677d5d9c
dancejms <dance.jms@gmail.com>
2017-09-23 19:50:06 +0800

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+
+#models.Model 表示 post是一个Django模型
+# foreignkey 指向另一个模型的连接
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(
+		default=timezone.now)
+	published_date = models.DateTimeField(
+		blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now
+		self.save()
+
+	def __str__(self):
+		return self.title
